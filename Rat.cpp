8bfe6b14c36773503fb77aaac7a6b1db5c4095a3
Andres Rios Tascon <ariostas@gmail.com>
2026-06-22 21:59:56 -0400

    Phase 1.1: Convert Rat.c to Rat.cpp with C linkage in Rat.h

--- Rat.cpp
+++ Rat.cpp
@@ -5,68 +5,43 @@ Rat  rI(Long a) 			       		      /*  a -> a/1  */
 {    Rat c; c.N=a; c.D=1; return c; 
 }
 Rat  rR(Long a, Long b)					    /* (a,b) -> a/b */
-{    Rat c; register Long g=Fgcd(a,b); 
-     if( (c.D=(b/g)) < 0 ) {c.D=-c.D; c.N=-a/g;} else c.N=a/g; 
-#ifdef	TEST
-     if(c.D<=0) {Rpr(c); puts(" *** c.D<=0 in rR! ***"); exit(0);}
-#endif
-     return c;
+{    Rat c; Long g=Fgcd(a,b); 
+     if( (c.D=(b/g)) < 0 ) {c.D=-c.D; c.N=-a/g;} else c.N=a/g; return c;
 } 
 Rat  irP(Long i, Rat c)					   /* (int) * (Rat) */
-{    register Long g=Fgcd(i,c.D); if(g < 0) g=-g; 
-     c.N *= (i/g); c.D/=g; 
-#ifdef	TEST
-     if(c.D<=0) {Rpr(c); puts(" *** c.D<=0 in irP! ***"); exit(0);}
-#endif
-     return c;
+{    Long g=Fgcd(i,c.D); if(g < 0) g=-g; 
+     c.N *= (i/g); c.D/=g; return c;
 } 
 Rat  rS(Rat a, Rat b)                                            /*  a + b  */
-{    Rat c; register Long g=Fgcd(a.D,b.D); 
+{    Rat c; Long g=Fgcd(a.D,b.D); 
      g = Fgcd(c.N=a.N*(b.D/g)+b.N*(a.D/g), c.D=a.D*(b.D/g));
      if(g < 0) g=-g;
-     c.N/=g; c.D/=g; /* LONG in line above ... */
-#ifdef	TEST
-     if(c.D<=0) {Rpr(c); puts(" *** c.D<=0 in rS! ***"); exit(0);}
-#endif
-     return c;
+     c.N/=g; c.D/=g; /* LONG in line above ... */return c;
 }
 Rat  rD(Rat a, Rat b)                                            /*  a - b  */
-{    Rat c; register Long g=Fgcd(a.D,b.D); 
+{    Rat c; Long g=Fgcd(a.D,b.D); 
      g = Fgcd(c.N=a.N*(b.D/g)-b.N*(a.D/g), c.D=a.D*(b.D/g));
      if(g < 0) g=-g;
-     c.N/=g; c.D/=g; /* LONG in line above ... */
-#ifdef	TEST
-     if(c.D<=0) {Rpr(c); puts(" *** c.D<=0 in rD! ***"); exit(0);}
-#endif
-     return c;
+     c.N/=g; c.D/=g; /* LONG in line above ... */return c;
 }
 Rat  rP(Rat a, Rat b)                                            /*  a * b  */
-{    register Long g=Fgcd(a.N,b.D); register Long h=Fgcd(b.N,a.D);
+{    Long g=Fgcd(a.N,b.D); Long h=Fgcd(b.N,a.D);
      Rat c; c.N=(a.N/g)*(b.N/h); 
      if((c.D=(a.D/h)*(b.D/g)) < 0) {c.D=-c.D; c.N=-c.N;} return c;
 }
 Rat  rQ(Rat a, Rat b)                                            /*  a / b  */
-{    register Long g=NNgcd(a.N,b.N); register Long h=Fgcd(b.D,a.D);
+{    Long g=NNgcd(a.N,b.N); Long h=Fgcd(b.D,a.D);
      Rat c; c.N=(a.N/g)*(b.D/h);
      if((c.D=(a.D/h)*(b.N/g)) < 0) {c.N=-c.N; c.D=-c.D;} return c;
 }
 int  rC(Rat a, Rat b)          /* Compare = [1 / 0 / -1] iff a [gt/eq/lt] b */
-{    register Long g=Fgcd(a.D,b.D);
+{    Long g=Fgcd(a.D,b.D);
      if((g=a.N*(b.D/g)-b.N*(a.D/g))) {if(g>0) return 1; else return -1;} 
      else return 0;
 }
 void Rpr(Rat c)					/* write "c.N/c.D" -> outFN */
 {    fprintf(outFILE,"%d/%d",(int) c.N, (int) c.D);
-} 
-#ifdef	TEST
-void TEST_Rat()
-{    Rat a, b; int i, j, k, l;
-     puts(" type 4 numbers: "); scanf("%d%d%d%d",&i,&j,&k,&l);
-     Rpr(rS(rR(i,j),rR(k,l))); puts(" i/j + k/l");
-     Rpr(rD(rR(i,j),rR(k,l))); puts(" i/j - k/l");
-}
-#endif
-/*  ==========  	  END of Rational Operations		==========  */
+} /*  ==========  	  END of Rational Operations		==========  */
 
 
 
@@ -78,12 +53,12 @@ void TEST_Rat()
  *	Egcd()  computes the EGCD for two integers
  *	REgcd() recursively computes the EGCD for a number of integers
  */
-Long Fgcd(register Long a, register Long b)     /* Fast greatest common div */
+Long Fgcd(Long a, Long b)     /* Fast greatest common div */
 {
      while( a %= b ) if ( !(b %= a) ) return a;
      return  b;
 }
-Long NNgcd(register Long a, register Long b)  /* NonNegative gcd handling 0 */
+Long NNgcd(Long a, Long b)  /* NonNegative gcd handling 0 */
 {
      a = (a<0) ? -a:a; b = (b<0) ? -b:b; if (!b) return a;
      while( a %= b ) if ( !(b %= a) ) return a;
@@ -95,8 +70,8 @@ Long NNgcd(register Long a, register Long b)  /* NonNegative gcd handling 0 */
  *   a_i==x_ia_0+y_ia_1a_i+1 and a_i+1=a_i-1 -(a_i-1 /a_i)a_i => 
  *   x_i+1=x_i-1 -(a_i-1 /a_i)x_i with x_0=1; x_1=0;  y_I=(a_I-a_0 x_I)/a_1
  *									    */
-Long Egcd(register Long A0, register Long A1, Long *Vout0, Long *Vout1)  
-{    register Long V0=A0, V1=A1, A2, X0=1, X1=0, X2=0;
+Long Egcd(Long A0, Long A1, Long *Vout0, Long *Vout1)  
+{    Long V0=A0, V1=A1, A2, X0=1, X1=0, X2=0;
      while((A2 = A0 % A1)) { X2=X0-X1*(A0/A1); A0=A1; A1=A2; X0=X1; X1=X2; }
      *Vout0=X1, *Vout1=(A1-(V0) * X1)/ (V1); return A1;
 }
@@ -169,68 +144,43 @@ LRat  LrI(LLong a) 			       		      /*  a -> a/1  */
 {    LRat c; c.N=a; c.D=1; return c; 
 }
 LRat  LrR(LLong a, LLong b)					    /* (a,b) -> a/b */
-{    LRat c; register LLong g=LFgcd(a,b); 
-     if( (c.D=(b/g)) < 0 ) {c.D=-c.D; c.N=-a/g;} else c.N=a/g; 
-#ifdef	TEST
-     if(c.D<=0) {LRpr(c); puts(" *** c.D<=0 in rR! ***"); exit(0);}
-#endif
-     return c;
+{    LRat c; LLong g=LFgcd(a,b); 
+     if( (c.D=(b/g)) < 0 ) {c.D=-c.D; c.N=-a/g;} else c.N=a/g; return c;
 } 
 LRat  LirP(LLong i, LRat c)					   /* (int) * (LRat) */
-{    register LLong g=LFgcd(i,c.D); if(g < 0) g=-g; 
-     c.N *= (i/g); c.D/=g; 
-#ifdef	TEST
-     if(c.D<=0) {LRpr(c); puts(" *** c.D<=0 in irP! ***"); exit(0);}
-#endif
-     return c;
+{    LLong g=LFgcd(i,c.D); if(g < 0) g=-g; 
+     c.N *= (i/g); c.D/=g; return c;
 } 
 LRat  LrS(LRat a, LRat b)                                            /*  a + b  */
-{    LRat c; register LLong g=LFgcd(a.D,b.D); 
+{    LRat c; LLong g=LFgcd(a.D,b.D); 
      g = LFgcd(c.N=a.N*(b.D/g)+b.N*(a.D/g), c.D=a.D*(b.D/g));
      if(g < 0) g=-g;
-     c.N/=g; c.D/=g; /* LLong in line above ... */
-#ifdef	TEST
-     if(c.D<=0) {LRpr(c); puts(" *** c.D<=0 in rS! ***"); exit(0);}
-#endif
-     return c;
+     c.N/=g; c.D/=g; /* LLong in line above ... */return c;
 }
 LRat  LrD(LRat a, LRat b)                                            /*  a - b  */
-{    LRat c; register LLong g=LFgcd(a.D,b.D); 
+{    LRat c; LLong g=LFgcd(a.D,b.D); 
      g = LFgcd(c.N=a.N*(b.D/g)-b.N*(a.D/g), c.D=a.D*(b.D/g));
      if(g < 0) g=-g;
-     c.N/=g; c.D/=g; /* LLong in line above ... */
-#ifdef	TEST
-     if(c.D<=0) {LRpr(c); puts(" *** c.D<=0 in rD! ***"); exit(0);}
-#endif
-     return c;
+     c.N/=g; c.D/=g; /* LLong in line above ... */return c;
 }
 LRat  LrP(LRat a, LRat b)                                            /*  a * b  */
-{    register LLong g=LFgcd(a.N,b.D); register LLong h=LFgcd(b.N,a.D);
+{    LLong g=LFgcd(a.N,b.D); LLong h=LFgcd(b.N,a.D);
      LRat c; c.N=(a.N/g)*(b.N/h); 
      if((c.D=(a.D/h)*(b.D/g)) < 0) {c.D=-c.D; c.N=-c.N;} return c;
 }
 LRat  LrQ(LRat a, LRat b)                                            /*  a / b  */
-{    register LLong g=LNNgcd(a.N,b.N); register LLong h=LFgcd(b.D,a.D);
+{    LLong g=LNNgcd(a.N,b.N); LLong h=LFgcd(b.D,a.D);
      LRat c; c.N=(a.N/g)*(b.D/h);
      if((c.D=(a.D/h)*(b.N/g)) < 0) {c.N=-c.N; c.D=-c.D;} return c;
 }
 int  LrC(LRat a, LRat b)          /* Compare = [1 / 0 / -1] iff a [gt/eq/lt] b */
-{    register LLong g=LFgcd(a.D,b.D);
+{    LLong g=LFgcd(a.D,b.D);
      if((g=a.N*(b.D/g)-b.N*(a.D/g))) {if(g>0) return 1; else return -1;} 
      else return 0;
 }
 void LRpr(LRat c)					/* write "c.N/c.D" -> outFN */
 {    fprintf(outFILE,"%lld/%lld",(LLong) c.N, (LLong) c.D);
-} 
-#ifdef	TEST
-void TEST_LRat()
-{    LRat a, b; int i, j, k, l;
-     puts(" type 4 numbers: "); scanf("%d%d%d%d",&i,&j,&k,&l);
-     LRpr(LrS(rR(i,j),LrR(k,l))); puts(" i/j + k/l");
-     LRpr(LrD(LrR(i,j),LrR(k,l))); puts(" i/j - k/l");
-}
-#endif
-/*  ==========  	  END of LRational OpeLRations		==========  */
+} /*  ==========  	  END of LRational OpeLRations		==========  */
 
 
 
@@ -242,12 +192,12 @@ void TEST_LRat()
  *	LEgcd()  computes the LEgcd for two integers
  *	LREgcd() recursively computes the LEgcd for a number of integers
  */
-LLong LFgcd(register LLong a, register LLong b)     /* Fast greatest common div */
+LLong LFgcd(LLong a, LLong b)     /* Fast greatest common div */
 {
      while( a %= b ) if ( !(b %= a) ) return a;
      return  b;
 }
-LLong LNNgcd(register LLong a, register LLong b)  /* NonNegative gcd handling 0 */
+LLong LNNgcd(LLong a, LLong b)  /* NonNegative gcd handling 0 */
 {
      a = (a<0) ? -a:a; b = (b<0) ? -b:b; if (!b) return a;
      while( a %= b ) if ( !(b %= a) ) return a;
@@ -259,8 +209,8 @@ LLong LNNgcd(register LLong a, register LLong b)  /* NonNegative gcd handling 0 
  *   a_i==x_ia_0+y_ia_1a_i+1 and a_i+1=a_i-1 -(a_i-1 /a_i)a_i => 
  *   x_i+1=x_i-1 -(a_i-1 /a_i)x_i with x_0=1; x_1=0;  y_I=(a_I-a_0 x_I)/a_1
  *									    */
-LLong LEgcd(register LLong A0, register LLong A1, LLong *Vout0, LLong *Vout1)  
-{    register LLong V0=A0, V1=A1, A2, X0=1, X1=0, X2=0;
+LLong LEgcd(LLong A0, LLong A1, LLong *Vout0, LLong *Vout1)  
+{    LLong V0=A0, V1=A1, A2, X0=1, X1=0, X2=0;
      while((A2 = A0 % A1)) { X2=X0-X1*(A0/A1); A0=A1; A1=A2; X0=X1; X1=X2; }
      *Vout0=X1, *Vout1=(A1-(V0) * X1)/ (V1); return A1;
 }
